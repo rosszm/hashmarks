c222e2ba76cf0e8d2454822f5529f2aba2c6aee2
Zack Ross <zmr462@usask.ca>
2022-04-15 23:15:44 -0600

    fix: update service

--- db/hockey-db-update.sh
+++ db/hockey-db-update.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # this file acts as an entrypoint to the update script.
-source /opt/hockey-db/env/bin/activate
-python3 /opt/hockey-db/hockey_db/update_service.py
+source /opt/hashmarks/db/env/bin/activate
+python3 /opt/hashmarks/db/hockey_db/update_service.py
