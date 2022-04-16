af21b1066d22ffe085976c917a4cdce860bcd4da
Zack Ross <zmr462@usask.ca>
2022-04-16 02:28:33 -0600

    fix: update gunicorn conf

--- api/gunicorn_conf.py
+++ api/gunicorn_conf.py
@@ -1,8 +1,10 @@
 from multiprocessing import cpu_count
 
 # socket
-bind = "127.0.0.1:8000"
+bind = "unix:/opt/hashmarks/api/gunicorn.sock"
 
 # Worker options
 workers = cpu_count() * 2 + 1
 worker_class = "uvicorn.workers.UvicornWorker"
+
+loglevel = "debug"
